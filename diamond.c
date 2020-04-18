528ab7decfa41251449f45d1491b62b2fc50fe0b
mharriso <julnovember@gmail.com>
2020-04-18 22:25:38 +0300

    add color)

--- diamond.c
+++ diamond.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#define RESET "\033[0m"
+#define CYAN "\033[1;36m"
 
 int printPyramid(int n, int amt)
 {
@@ -23,7 +25,7 @@ int printPyramid(int n, int amt)
 			int x = y - 1;
 			while (x != 0)
 	    	{
-			printf("%2d", y);
+			printf("%s%2d%s", CYAN, y, RESET);
 			x--;
 			}
 			i++;
@@ -37,6 +39,40 @@ int printPyramid(int n, int amt)
 }
 
 int printRevPyramid(int n, int amt)
+{
+	int y = 1;
+	while (y <= n - 2)
+	{
+		int i = 1;
+		char spaces[n];
+		while (i <= amt)
+		{
+			if (i == 1)
+			{
+				sprintf(spaces, "%%%dd", y + 2);
+			    printf(spaces, n - y);
+			}
+		    else
+			{
+				sprintf(spaces, "%%%dd", (y + 1)*2);
+			    printf(spaces, n - y);
+			}
+			int x = n - 1 - y;
+			while (x != 0)
+	        {
+			printf("%s%2d%s", CYAN, n - y, RESET);
+			x--;
+		    }
+			i++;
+		}
+		printf("\n");
+		y++;
+	}
+	return (0);
+
+}
+
+int printRevPyramidEnd(int n, int amt)
 {
 	int y = 1;
 	while (y <= n - 1)
@@ -58,7 +94,7 @@ int printRevPyramid(int n, int amt)
 			int x = n - 1 - y;
 			while (x != 0)
 	        {
-			printf("%2d", n - y);
+			printf("%s%2d%s", CYAN, n - y, RESET);
 			x--;
 		    }
 			i++;
@@ -76,10 +112,17 @@ int diamond(int n, int amt)
 	printRevPyramid(n, amt);
 	return (0);
 }
+
+int diamondEnd(int n, int amt)
+{
+	printPyramid(n, amt);
+	printRevPyramidEnd(n, amt);
+	return (0);
+}
 int main()
 {
 	int amt,lines,n;
-	printf("\nПривет!)\n\nБудем делать узор!\n\nКакой будет размер одного элемента? : ");
+	printf("\nПривет!) Будем делать узор!\n\nКакой будет размер одного элемента? : ");
 	scanf("%d", &n);
 	printf("\nВведите, пожалуйста, количество в строке: ");
 	scanf("%d", &amt);
@@ -91,10 +134,11 @@ int main()
 	if (n == 0)
 		printf("What did you expect to see?) ^_^\n");
 	if (n > 0 & lines > 0)
-		while(lines != 0)
+		while(lines != 1)
 		{
 			diamond(n, amt);
 			lines--;
 		}
+		diamondEnd(n, amt);
 	return (0);
 }
